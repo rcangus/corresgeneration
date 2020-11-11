e3c3e294d793b02bc1d3ffa1458450f26fe3c5e5
Richard Angus <48549112+rcangus@users.noreply.github.com>
2020-11-11 08:16:48 +0000

    Added NoRetry to  interfaces

--- Logging.Services.Shared/Logging.Services.Shared/Interfaces/ICustomLogger.cs
+++ Logging.Services.Shared/Logging.Services.Shared/Interfaces/ICustomLogger.cs
@@ -11,11 +11,13 @@ namespace Logging.Services.Shared.Interfaces
         [Queue(CustomLoggerQueues.CustomLoggerHighPriorityQueueLogMessage)]
         [JobDisplayName("CustomLogger HighPriority Start - {0}")]
         [DisplayName("CustomLogger HighPriority Start - {0}")]
+        [AutomaticRetry(Attempts = 0)]
         bool LogMessage(LogMsg msg, PerformContext context);
 
         [Queue(CustomLoggerQueues.CustomLoggerLowPriorityQueueLogMessage)]
         [JobDisplayName("CustomLogger LowPriority Start - {0}")]
         [DisplayName("CustomLogger LowPriority Start - {0}")]
+        [AutomaticRetry(Attempts = 0)]
         bool LogMessageLowPriority(LogMsg msg, PerformContext context);
 
         bool AddSoWeCanGetANewVersion();
