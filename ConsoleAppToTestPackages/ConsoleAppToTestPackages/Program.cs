06ad4326751678371c83db71f9713c5204c7f900
Richard Angus <richard.angus@live.com.au>
2020-11-16 15:55:56 +1000

    Added reeference to package

---
+++ ConsoleAppToTestPackages/ConsoleAppToTestPackages/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppToTestPackages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
